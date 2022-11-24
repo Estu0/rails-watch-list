f20b20c9f054c17a11a7a0bd17a0010680748148
EstuardoJoel HiguerosReyes <estuar0jhr@gmail.com>
2022-11-24 17:34:01 -0500

    Watch List

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :lists do
     resources :bookmarks, only: [:new, :create]
   end
+
+  resources :bookmarks, only: [:destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
