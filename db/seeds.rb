f38a3fc6389c6ff15d7fd0d7e471a127052c1f78
EstuardoJoel HiguerosReyes <estuar0jhr@gmail.com>
2022-11-25 16:51:26 -0500

    Watch List Functional

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 puts "Cleaning DB..."
-Movie.destroy_all
-List.destroy_all
+# Movie.destroy_all
+# List.destroy_all
 
 puts "Creating objects.."
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
